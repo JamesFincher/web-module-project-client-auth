e12d1bd67916f236070208af0fac007437f63403
heartandkarma <heartandkarma@gmail.com>
2022-04-26 11:29:10 -0700

    got Login setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,38 @@
 import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const { push } = useHistory();
+  const [cred, setCredit] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setCredit({
+      ...cred,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:9000/api/login", cred).then((res) => {
+      localStorage.setItem("token", res.payload);
+      push("/friends");
+    });
+  };
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username</label>
-          <input id="username" />
+          <input id="username" onChange={handleChange} name="username" />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input id="password" />
+          <input id="password" onChange={handleChange} name="password" />
         </div>
         <button>Submit</button>
       </form>
